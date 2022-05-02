8d8cf8cfa9cdd7701eb43613d01c054475d59445
Agustín Salazar <agustin.salazar@utp.edu.co>
2022-05-02 12:09:15 -0500

    Initial commit

---
+++ src/test/java/usersms/usersms/UsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package usersms.usersms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
